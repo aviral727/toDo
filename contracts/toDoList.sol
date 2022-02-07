de1376f926d6b5a503f47c5313560a0430f418f8
aviral727 <aviralraj20142222@gmail.com>
2022-02-07 20:34:10 +0530

    first commit

---
+++ contracts/toDoList.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract ToDoList {
+    uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    constructor() {
+        createTask("Check us out");
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+}
